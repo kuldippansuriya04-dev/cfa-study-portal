06583cfad92fd0191b8bd84c7927f6c821124aaf
kuldippansuriya04-dev <kuldippansuriya04@gmail.com>
2026-06-08 10:35:07 +0000

    ultimate fix

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 const app = document.getElementById('app')
 
 app.innerHTML = `
-<div style="font-family: Arial; padding:40px">
+<div style="font-family: Arial; padding: 40px">
   <h1>CFA Study Portal</h1>
-  <p>Website successfully deployed 🚀</p>
 
   <h2>Flashcards</h2>
+
   <ul>
-    <li><a href="./flashcards/ethics.html">Ethics</a></li>
     <li><a href="./flashcards/economics.html">Economics</a></li>
+    <li><a href="./flashcards/ethics.html">Ethics</a></li>
     <li><a href="./flashcards/equity.html">Equity</a></li>
     <li><a href="./flashcards/fixed-income.html">Fixed Income</a></li>
+    <li><a href="./flashcards/derivatives.html">Derivatives</a></li>
+    <li><a href="./flashcards/fsa.html">FSA</a></li>
+    <li><a href="./flashcards/pm.html">Portfolio Management</a></li>
+    <li><a href="./flashcards/qm.html">Quant Methods</a></li>
   </ul>
 </div>
 `
